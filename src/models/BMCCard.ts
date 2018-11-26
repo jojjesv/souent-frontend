5ef2d6d5c4d2c0a16bb7a7463dcec4e90e621f72
Johan Svensson <partlight.johans@gmail.co>
2018-11-26 13:39:55 +0100

    added models for enterprise + bmc cards

---
+++ src/models/BMCCard.ts
@@ -0,0 +1,9 @@
+/**
+ * Represents a single BMC card for an enterprise.
+ * @author Johan Svensson
+ */
+export default class {
+  public title: string;
+  public symbolImageUrl: string;
+  public htmlContent: string;
+}
---
+++ src/models/Enterprise.ts
@@ -0,0 +1,10 @@
+import BMCCard from "./BMCCard";
+
+/**
+ * Represents a single enterprise with a BMC.
+ * @author Johan Svensson
+ */
+export default class {
+  id: string;
+  cards: BMCCard[];
+}
