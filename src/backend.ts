3fcff4c511d2c0955b6fc588b84c275d1f53e931
Johan Svensson <partlight.johans@gmail.co>
2018-11-30 11:26:24 +0100

    added backend and service

---
+++ src/backend.ts
@@ -0,0 +1,43 @@
+/**
+ * Standard backend communication functions.
+ * @author Johan Svensson
+ */
+;
+
+//@ts-ignore
+import fetch from 'whatwg-fetch';
+
+let baseUrl = `http://localhost:3000/api/`;
+
+/**
+ * Performs a standard backend request.
+ * Expects the result as JSON!
+ */
+export async function request(path: string, method: 'get'|'post'|'put'|'delete', body: any = null, query: string = null) {
+  path = path || "";
+  query = query || "";
+
+  if (path.startsWith("/")) {
+    path = path.substr(1);
+  }
+
+  if (!query.startsWith("?")) {
+    query = `?${query}`;
+  }
+
+  if (body && typeof body == "object") {
+    body = JSON.stringify(body);
+  }
+  
+  if (method == "get") {
+    body = undefined;
+  }
+
+  let url = `${baseUrl}${path}${query}`;
+  let result = await fetch(url, {
+    method,
+    body
+  });
+
+  return result.json();
+}
