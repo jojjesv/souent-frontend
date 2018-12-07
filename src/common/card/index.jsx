186c833d6148a38072c3c9fc13b00e564dfa0fbf
Johan Svensson <partlight.johans@gmail.co>
2018-12-07 14:47:58 +0100

    card uses dangerouslySetHTML

--- src/common/card/index.jsx
+++ src/common/card/index.jsx
@@ -6,7 +6,7 @@ export default function Card(props){
     return <div className="card" style={{top: props.dt, height: props.ch}}>
         <img src={props.cimg} className="symbol top-left"></img>
         <h1 className="header" id="b2">{props.htext}</h1>
-        <p className="preview-content" id="b3">{props.ptext}</p>
+        <p className="preview-content" id="b3" dangerouslySetInnerHTML={{ __html: props.ptext }}></p>
         <button className="open" value={props.id} onClick={props.onOpen}>Open</button>
         <img src={props.cimg} className="symbol bottom-right" style={{transform: props.deg}}></img>
     </div>
