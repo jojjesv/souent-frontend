8bae458017225dda67694070d41e78586d5074bb
Johan Svensson <partlight.johans@gmail.co>
2018-12-07 15:49:05 +0100

    rotated desktop cards

--- src/common/card_list/index.jsx
+++ src/common/card_list/index.jsx
@@ -10,16 +10,20 @@ export default class CardList extends React.Component {
 
     let cards = [];
     for (let i = 0; i < cardsData.length; i++) {
-      cards.push(<Card
-        key={i}
-        id={i}
-        ch={this.props.cHeight}
-        deg={"rotate(" + this.props.imgDeg + "deg)"}
-        cimg={`assets/images/${this.cardType(i)}.jpg`}
-        dt={this.props.dTop + i * this.props.dAdd}
-        htext={cardsData[i].title}
-        onOpen={() => props.onCardOpen(cardsData[i], i)}
-        ptext={cardsData[i].htmlPreviewContent} />);
+      cards.push((
+        <div className="card-container-single">
+          <Card
+            key={i}
+            id={i}
+            ch={this.props.cHeight}
+            deg={"rotate(" + this.props.imgDeg + "deg)"}
+            cimg={`assets/images/${this.cardType(i)}.jpg`}
+            dt={this.props.dTop + i * this.props.dAdd}
+            htext={cardsData[i].title}
+            onOpen={() => props.onCardOpen(cardsData[i], i)}
+            ptext={cardsData[i].htmlPreviewContent} />
+        </div>
+      ));
     }
     return (
       <div className="card-list">
