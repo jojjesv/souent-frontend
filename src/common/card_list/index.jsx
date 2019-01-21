be7e68edb4a7fb5806bef662540ba933301fbe8c
Johan Svensson <partlight.johans@gmail.co>
2019-01-21 11:34:51 +0100

    absolute path for symbol images

--- src/common/card_list/index.jsx
+++ src/common/card_list/index.jsx
@@ -17,7 +17,7 @@ export default class CardList extends React.Component {
             key={i}
             cardHeight={props.cardHeight}
             topDistance={props.topDistance + 1 * props.addDistance}
-            img={`assets/images/${i}.png`}
+            img={`/assets/images/${i}.png`}
             headerText={cardsData[i].title}
             openLinkHref={props.openLinkHref ? props.openLinkHref(cardsData[i]) : null}
             open={() => props.onCardOpen(cardsData[i], i)}
